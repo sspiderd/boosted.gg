8ef048b064161c20e2cd3369da61d548b6f369c7
Ilan Goldenstein <angeli106@gmail.com>
2016-08-11 17:37:15 +0300

    a commit
    Signed-off-by: Ilan Goldenstein <angeli106@gmail.com>

---
+++ data-maker/src/main/groovy/gg/masters/FromFile.groovy
@@ -0,0 +1,33 @@
+package gg.masters
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+/**
+ * Created by ilan on 8/11/16.
+ */
+class FromFile {
+
+    /* Returns a list maps, each containing:
+     *
+     * MatchId
+     * SummonerId
+     * Champion
+     * Role
+     * Win/Loss
+    */
+    static List<SummonerGame> parseMatch(match) {
+        println JsonOutput.prettyPrint(JsonOutput.toJson(match))
+    }
+
+    public static void main(String[] args) {
+        String matchesText = this.getClass().getResource( '/matches1.json' ).openStream().text
+        new JsonSlurper().parseText(matchesText)['matches'].each { match ->
+            List<SummonerGame> parsed = MatchParser.parseMatch(match) ;
+            parsed.each {
+                KafkaSummonerGameProducer.send(it)
+            }
+        }
+    }
+
+}
