ae56b86d9eba03e9d8e723e1852012cf0f4c4263
ShinySpiderdude <angeli106@gmail.com>
2016-08-23 22:51:55 +0300

    changed image to xenial

--- Vagrantfile
+++ Vagrantfile
@@ -7,25 +7,27 @@
 # you're doing.
 Vagrant.configure(2) do |config|
   
-  config.vm.box = "ubuntu/trusty64"
-  #config.vm.box = "puppetlabs/debian-8.2-64-nocm"
+  config.vm.box = "ubuntu/xenial64"
 
   config.vm.provider "virtualbox" do |v|
    v.customize ["modifyvm", :id, "--nictype1", "virtio"]
    v.customize ["modifyvm", :id, "--cpuexecutioncap", "80"]
-   v.memory = 4096
+   v.memory = 2048
    v.cpus = 4
   end
 
   config.vm.network "private_network", ip: "10.0.0.3", nic_type: "virtio"
+
+  #For the day i'll be using docker for kafka and zk
   #config.vm.network "forwarded_port", guest: 9092, host: 9092
   #config.vm.network "forwarded_port", guest: 2181, host: 2181
 
-  config.vm.provision "shell", path: "provision/provision.sh"
-
   ["zookeeper", "kafka", "docker-containers"].each do |service|
-	config.vm.provision "file", source: "provision/#{service}.conf", destination: "/tmp/#{service}.conf"
+  #I have to copy it to /tmp and later copy it with the provision_late.sh since the "file" provisioner is done with the vagrant user, rather than root
+    config.vm.provision "file", source: "provision/#{service}.service", destination: "/tmp/#{service}.service"
   end
-  config.vm.provision "shell", path: "provision/provision_late.sh"
+  config.vm.provision "file", source: "provision/cassandra.init", destination: "/tmp/cassandra.init"
+
+  config.vm.provision "shell", path: "provision/provision.sh"
 
 end
--- provision/provision.sh
+++ provision/provision.sh
@@ -5,28 +5,46 @@ SCALA_VERSION=2.11
 KAFKA_VERSION=0.10.0.1
 DEBIAN_FRONTEND=noninteractive
 
+echo "127.0.1.1 ubuntu-xenial" >> /etc/hosts
+
 echo updating and adding repositories
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:brightbox/ruby-ng
+#apt-get install -y software-properties-common
+#add-apt-repository -y ppa:brightbox/ruby-ng
 
 #java 8
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-add-apt-repository -y ppa:webupd8team/java
+#echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+#add-apt-repository -y ppa:webupd8team/java
 
-apt-get update && apt-get install -y ca-certificates apt-transport-https vim curl linux-image-extra-$(uname -r) ruby2.3 ruby2.3-dev rake ansible subversion oracle-java8-installer oracle-java8-set-default maven
+apt-get update && apt-get install -y ca-certificates apt-transport-https vim curl linux-image-extra-$(uname -r) openjdk-8-jre 
+	#ruby2.3 ruby2.3-dev \
 
 curl https://get.docker.com | sh > /dev/null
-usermod vagrant -a -G docker
+usermod ubuntu -a -G docker
 sed -i 's/\"set background=dark/set background=dark/' /etc/vim/vimrc
 
 #kafka
 curl -SL http://www-eu.apache.org/dist/kafka/$KAFKA_VERSION/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz | tar xz -C /opt
 ln -s /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/ /opt/kafka
 
-mkdir -p /var/log/zookeeper /var/log/kafka
+mkdir -p /var/log/zookeeper /var/log/kafka /usr/lib/systemd/system /opt/cassandra-init
 
 sed -i 's@#listeners=PLAINTEXT://:9092@listeners=PLAINTEXT://10.0.0.3:9092@' /opt/kafka/config/server.properties
 
-
-
 #docker build -t kafka /dockerfiles/kafka-zookeeper && docker run -d -p 2181:2181 -p 9092:9092 kafka 
+
+mv /tmp/*service /usr/lib/systemd/system
+mv /tmp/cassandra.init /opt/cassandra-init
+
+systemctl enable zookeeper kafka docker-containers
+systemctl start zookeeper kafka
+
+docker run -d -v /var/lib/cassandra:/var/lib/cassandra -v /opt/cassandra-init:/opt/cassandra-init -p 9042:9042 --name cassandra cassandra
+
+#let the service start
+sleep 15
+
+#Create a topic
+/opt/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic boostedgg
+
+#Create cassandra keyspace and tables
+docker exec cassandra cqlsh -f /opt/cassandra-init/cassandra.init
